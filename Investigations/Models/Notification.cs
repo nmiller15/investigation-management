26ce586839c94bc702e5646e3df655851e8952db
Nolan Miller <nolan.miller77@gmail.com>
2026-02-19 05:55:43 -0500

    add notification parser

--- Investigations/Models/Notification.cs
+++ Investigations/Models/Notification.cs
@@ -2,10 +2,10 @@ namespace Investigations.Models;
 
 public class Notification : BaseAuditModel
 {
-    public Guid NotificationId { get; set; } = Guid.Empty;
+    public int NotificationKey { get; set; } = 0;
     public string Subject { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Guid AssignedToUserId { get; set; } = Guid.Empty;
+    public int AssignedToUserKey { get; set; } = -1;
     public string AssignedToUserFirstName { get; set; } = string.Empty;
     public string AssignedToUserLastName { get; set; } = string.Empty;
     public bool IsDelayed { get; set; }
